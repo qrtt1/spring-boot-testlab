f6b46c3d5c2e2a978863782ea1a5e81c9d6e0e23
Ching Yi, Chan <chingyichan.tw@gmail.com>
2020-12-01 21:11:37 +0800

    init project
    Signed-off-by: Ching Yi, Chan <chingyichan.tw@gmail.com>

---
+++ src/main/kotlin/org/qrtt1/springboottestlab/SpringBootTestlabApplication.kt
@@ -0,0 +1,11 @@
+package org.qrtt1.springboottestlab
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootTestlabApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBootTestlabApplication>(*args)
+}
