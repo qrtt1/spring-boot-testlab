d962fd14e6874c1ca6511d5e432199d10f31b5c0
Ching Yi, Chan <chingyichan.tw@gmail.com>
2020-12-01 22:32:48 +0800

    Add java-version to martix
    Signed-off-by: Ching Yi, Chan <chingyichan.tw@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,7 @@ dependencies {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "11"
+        jvmTarget =  (System.getenv("JavaVersion") ?: "11")
     }
 }
 
